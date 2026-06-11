f53574e1219fd355e4ad1b5a083cee744f64fb23
SK <23052428@kiit.ac.in>
2026-06-11 13:11:18 +0530

    Delete ad directory

--- ad/backend/models/Answer.js
+++
@@ -1,5 +0,0 @@
-module.exports = mongoose.model("Answer", new mongoose.Schema({
-  studentId:{type:mongoose.Schema.Types.ObjectId, ref:"User"},
-  questionId:{type:mongoose.Schema.Types.ObjectId, ref:"Question"},
-  answerText:String
-}))
--- ad/backend/models/Question.js
+++
@@ -1,7 +0,0 @@
-module.exports = mongoose.model("Question", new mongoose.Schema({
-  title:String,
-  questionText:String,
-  maxMarks:Number,
-  subject:String,
-  createdBy:{type:mongoose.Schema.Types.ObjectId, ref:"User"}
-}))
